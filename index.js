7729c42872903e188ad8e5f8f40b909d0fd16d7f
Chris <chris@solaris.lan>
2023-01-26 09:49:08 -0500

    added comments

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const ws = require('ws');
-const http = require('http');
 const app = require('./app');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const { WSServer } = require('./utils/websocket');
-// const initiateWebSocket = require('./controllers/web-socket');
-// const wsListener = new ws.WebSocketServer({ noServer: true });
-
-// const server = http.createServer(app);
-// initiateWebSocket(server);
 
 const server = app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}...`);
 });
 
+// when App server receives request to 'upgrade' to WebSocket protocol
+// (a client sends a request to connect via WebSocket)
 server.on('upgrade', (request, socket, head) => {
+  // WebSocket Server handles the upgrade event
   WSServer.handleUpgrade(request, socket, head, (ws) => {
+    //WebSocket Server opens up a socket, triggers the 'connection' event
     WSServer.emit('connection', ws, request);
   });
 });
--- utils/websocket.js
+++ utils/websocket.js
@@ -1,14 +1,17 @@
 const ws = require('ws');
 const logger = require('./logger')
 
+// initialize a WebSocket Server that works on the same port with App server
 const WSServer = new ws.WebSocketServer({ noServer: true });
 const Sockets = {};
 
+// When a client establishes a connection with WebSocket Server:
 WSServer.on('connection', (socket, request) => {
-  const url = request.headers.host;
+  const url = request.headers.host; // define unique client identifier here
   Sockets[url] = socket;
   logger.info('WebSocket connected with URL: ' + url)
 
+  // removes socket from Sockets collection when client disconnects
   socket.on('close', () => {
     delete Sockets[url];
     logger.info('WebSocket client disconnected, delete ' + url);
