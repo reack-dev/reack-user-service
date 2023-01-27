4fe4ebb57fbca563c28544514dddc516162284c6
Chris <chris@solaris.lan>
2023-01-27 00:46:28 -0500

    fixed data being sent to the front-end

--- controllers/new-request.js
+++ controllers/new-request.js
@@ -6,7 +6,7 @@ newRequestRouter.post('/newRequest', async (req, res, next) => {
   const randomString = req.body.randomString
 
   if (Sockets && Sockets[randomString]) { // look for current active WebSocket connections
-    Sockets[randomString].send(JSON.stringify(req.body));
+    Sockets[randomString].send(JSON.stringify(req.body.requests));
     res.status(200).send({ message: 'Forwarded to front-end' });
   } else { // no matching connection found
     res.status(400).send({ message: 'Client not connected with WebSocket or not found, payload not forwarded'})
